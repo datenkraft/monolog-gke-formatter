06ef064641e165c5f80dc1f9696d990db82e8c91
rlehner-dk <103114558+rlehner-dk@users.noreply.github.com>
2023-06-09 12:56:20 +0200

    BB-4579 - Refactor GkeFormatter.php for monolog 3.x compatibility
    * BB-4579 - Add composer.lock
    
    * BB-4579 - Refactor GkeFormatter.php for monolog 3.x compatibility
    
    * BB-4579 - Refactor GkeFormatter.php for monolog 3.x compatibility
    
    * BB-4579 - Refactor GkeFormatter.php for monolog 3.x compatibility

--- src/GkeFormatter.php
+++ src/GkeFormatter.php
@@ -1,18 +1,27 @@
 <?php
 
+declare(strict_types=1);
+
 namespace Datenkraft\MonologGkeFormatter;
 
 use GuzzleHttp\Psr7\ServerRequest;
 use Monolog\Formatter\JsonFormatter;
+use Monolog\LogRecord;
+use RuntimeException;
 
 class GkeFormatter extends JsonFormatter
 {
-    const BACKTRACE_DEFAULT_CALL = 6;
+    public const BACKTRACE_DEFAULT_CALL = 6;
 
-    protected $deepToBacktrace;
-    protected $httpRequestContext;
-    protected $sourceLocationContext;
+    protected int $deepToBacktrace;
+    protected bool $httpRequestContext;
+    protected bool $sourceLocationContext;
 
+    /**
+     * @param self::BATCH_MODE_* $batchMode
+     *
+     * @throws RuntimeException If the function json_encode does not exist
+     */
     public function __construct(
         int $batchMode = self::BATCH_MODE_JSON,
         bool $appendNewline = true,
@@ -25,54 +34,63 @@ class GkeFormatter extends JsonFormatter
         $this->httpRequestContext = $httpRequestContext;
         $this->sourceLocationContext = $sourceLocationContext;
         $this->deepToBacktrace = $deepToBacktrace;
-        
-        $this->includeStacktraces(true);
+
+        $this->includeStacktraces();
     }
 
-    public function format(array $record): string
+    /**
+     * @inheritDoc
+     */
+    public function format(LogRecord $record): string
     {
         $request = ServerRequest::fromGlobals();
-        $debug = debug_backtrace(DEBUG_BACKTRACE_IGNORE_ARGS,$this->deepToBacktrace);
+        $debug = debug_backtrace(DEBUG_BACKTRACE_IGNORE_ARGS, $this->deepToBacktrace);
+        $context = array_merge(
+            $this->sourceLocationContext && isset($debug[$this->deepToBacktrace - 2])
+                ? [
+                'sourceLocation' => [
+                    'file' => $debug[$this->deepToBacktrace - 2]['file'],
+                    'line' => $debug[$this->deepToBacktrace - 2]['line'],
+                    'function' => isset($debug[$this->deepToBacktrace - 1]['function']) && isset($debug[$this->deepToBacktrace - 1]['class'])
+                        ? $debug[$this->deepToBacktrace - 1]['class'] . $debug[$this->deepToBacktrace - 1]['type'] . $debug[$this->deepToBacktrace - 1]['function']
+                        : (
+                            $debug[$this->deepToBacktrace - 1]['function'] ?? ''
+                        ),
+                ]
+            ]
+                : [],
+            $this->httpRequestContext && str_contains(php_sapi_name(), 'cgi')
+                ? [
+                'httpRequest' => [
+                    'requestMethod' => $request->getMethod(),
+                    'requestUrl' => $request->getUri()->__toString(),
+                    'requestSize' => $request->getBody()->getSize(),
+                    'protocol' => $request->getProtocolVersion(),
+                    'referer' => $request->getHeaderLine('Referer'),
+                    'userAgent' => $request->getHeaderLine('User-Agent'),
+                    'remoteIp' => $request->getHeaderLine('X-Forwarded-For'),
+                ],
+            ]
+                : [],
+            [
+                'message' => $record->message,
+                'thread' => $record->channel,
+                'severity' => $record->level->getName(),
+                'serviceContext' => $record->context,
+                'timestamp' => $record->datetime->getTimestamp(),
+            ]
+        );
 
         return parent::format(
-            array_merge(
-                $record['extra'],
-                $this->sourceLocationContext && isset($debug[$this->deepToBacktrace-2])
-                    ? [
-                        'sourceLocation' => [
-                            'file' => $debug[$this->deepToBacktrace-2]['file'],
-                            'line' => $debug[$this->deepToBacktrace-2]['line'],
-                            'function' => isset($debug[$this->deepToBacktrace-1]) && isset($debug[$this->deepToBacktrace-1]['class']) && isset($debug[$this->deepToBacktrace-1]['function'])
-                                ? $debug[$this->deepToBacktrace-1]['class'] . $debug[$this->deepToBacktrace-1]['type'] . $debug[$this->deepToBacktrace-1]['function']
-                                : (
-                                isset($debug[$this->deepToBacktrace-1]) && isset($debug[$this->deepToBacktrace-1]['function'])
-                                    ? $debug[$this->deepToBacktrace-1]['function']
-                                    : ''
-                                ),
-                        ]
-                    ]
-                    : [],
-                $this->httpRequestContext && preg_match('/cgi/', php_sapi_name())
-                    ? [
-                        'httpRequest' => [
-                            'requestMethod' => $request->getMethod(),
-                            'requestUrl' => $request->getUri()->__toString(),
-                            'requestSize' => $request->getBody()->getSize(),
-                            'protocol' => $request->getProtocolVersion(),
-                            'referer' => $request->getHeaderLine('Referer'),
-                            'userAgent' => $request->getHeaderLine('User-Agent'),
-                            'remoteIp' => $request->getHeaderLine('X-Forwarded-For'),
-                        ],
-                    ]
-                    : [],
-                [
-                    'message' => $record['message'],
-                    'thread' => $record['channel'],
-                    'severity' => $record['level_name'],
-                    'serviceContext' => $record['context'],
-                    'timestamp' => $record['datetime']->getTimestamp(),
-                ]
-            )
+            (new LogRecord(
+                $record->datetime,
+                $record->channel,
+                $record->level,
+                $record->message,
+                $context,
+                $record->extra,
+                $record->formatted
+            ))
         );
     }
 }
